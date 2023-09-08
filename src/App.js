d51737c6436c84672ec6a789cbb1629c491a866e
Monisha Paranjothi <monisha.paranjothi@gswvss.com>
2023-09-08 14:56:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const people =[
+    // {name: 'John'},
+    // {name: 'Jane'},
+    // {name: 'Adam'}
+
+    // {id:1,name: 'John'},
+    // {id:2,name: 'Jane'},
+    // {id:3,name: 'Adam'}
+
+
+    
+    {
+      name: 'Adam',
+      skills:[
+        {name:'Angular',type:'frontend'},
+        {name:'React',type:'frontend'},
+      ] 
+     },
+     {
+       name: 'Nick',
+       skills:[
+         {name:'NodeJS',type:'backend'},
+         {name:'Spring',type:'backend'},
+       ] 
+      }
+    
+
+  ];
+
+  // const dataCollection = [{
+  //   id:1,
+  //   title:'ReactJS Tamil Training',
+  //   label:'MaanavaN Learn Code'
+  // }];
+ 
+    const dataCollection = [];
+  let ListTemplate;
+
+
+  if(dataCollection.length){
+    ListTemplate = dataCollection.map((item,index) =>
+  <li key={index}>{item.title}- {item.label}</li>
+    );
+  }
+  else{
+          ListTemplate = <li>No Messages Found</li>
+  } 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <div>  {people.map((person,index) => (
+            <div key={index}>
+                    <h2>{person.name}</h2> 
+                <div>
+            {person.skills.map((skill,i)=>(
+              <p key={i}>
+                {skill.type} named {skill.name}
+                </p>
+            ))}
+                </div>
+                </div>
+
+        ))}</div> */}
+
+        <ul>{ListTemplate}</ul>
       </header>
-    </div>
-  );
+      </div>);
 }
 
 export default App;
